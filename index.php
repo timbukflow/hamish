07550544c9dea47ac31c032b37ff71baab590d5d
Ivo Schwizer <ivoschwizer@gmail.com>
2024-07-26 14:46:31 +0200

    first

---
+++ index.php
@@ -0,0 +1,64 @@
+<!--
+
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+Made by Schwizer Design GmbH
+Say hello @ schwizerdesign.ch
+Version 1.0
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+-->
+
+<!DOCTYPE html>
+<html lang="de">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# place: http://ogp.me/ns/place#">
+
+    <meta charset="UTF-8">
+    <title>HAMISH</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="HAMISH">
+    <meta name="keywords" content="HAMISH">
+
+    <meta name="author" content="HAMISH">
+    <link rel="canonical" href="https://hamish.ch/" />
+
+    <meta name="robots" content="index, follow" /> 
+    <meta http-equiv="cache-control" content="public, max-age=3600" />
+    <meta http-equiv="pragma" content="cache" />
+
+    <link rel="icon" type="image/svg+xml" href="/img/favicon.svg">
+    <link rel="icon" type="image/png" href="/img/favicon.png">
+
+    <meta property="og:title" content="HAMISH">
+    <meta property="og:description" content="HAMISH">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://hamish.ch/">
+    <meta property="og:image" content="https://hamish.ch/img/og-image.jpg">
+    <meta property="og:locale" content="de_CH">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="HAMISH">
+    <meta name="twitter:description" content="IHAMISH">
+    <meta name="twitter:image" content="https://hamish.ch/img/twitter-image.jpg">
+
+    <meta name="format-detection" content="telephone=yes">
+    <!-- <meta property="business:contact_data:street_address" content="Buchstrasse 27">
+    <meta property="business:contact_data:locality" content="St.Gallen"> 
+    <meta property="business:contact_data:region" content="9000">
+    <meta property="business:contact_data:postal_code" content="9000">
+    <meta property="business:contact_data:country_name" content="Switzerland"> -->
+    
+    <link rel="stylesheet" href="main.css" />
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="main.js"></script>  
+</head>
+
+<body>
+  <div class="holder">
+      <img src="img/hamish-logo.svg" alt="hamish-logo">
+  </div>  
+</body>
+</html>
